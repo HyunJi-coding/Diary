4b501dd18ab22d41fd6f0f5b15bc63a030aa0798
Hyunji-coding <dnflsms3@naver.com>
2024-02-29 18:59:09 +0900

    entity

---
+++ src/main/java/org/example/Note.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Note {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    @Lob
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public Note(
+            String title,
+            String content
+    ) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void updateContent(
+            String title,
+            String content
+    ) {
+        this.title = title;
+        this.content = content;
+    }
+}
