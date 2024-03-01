32edf417b0800f6c4af748954f9ce80137549250
Hyunji-coding <dnflsms3@naver.com>
2024-03-02 00:43:49 +0900

    dto

---
+++ src/main/java/org/example/dto/UserDto.java
@@ -0,0 +1,11 @@
+package org.example.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDto {
+    private String username;
+    private String password;
+}
